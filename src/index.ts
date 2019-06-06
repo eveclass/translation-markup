aecfe18c165a5324420a0acd51db8aaf67c4789c
Mario <mario@macbook-mario.local>
2019-06-06 16:48:20 -0300

    adiçao de export dos tipos no index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './model/Engine';
 export * from './model/Translator';
 export * from './model/Compiler';
+export * from './enums/FormatOptions';
+export * from './interfaces/ICompileOptions';
